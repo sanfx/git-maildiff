be0c1864d3d71b60d410c918fd51e4c15a14ee5c
san <skysan@gmail.com>
2014-01-26 21:49:45 +0530

    fixed attachment check

--- send.py
+++ send.py
@@ -61,11 +61,12 @@ class EMail(object):
 			#the Body message
 			msg.attach(MIMEText(msgHTML, 'html'))
 			msg.attach(MIMEText("Sent using git ipush\n git clone https://sanfx@bitbucket.org/sanfx/git-ipush.git"))
-			for phile in attachments:
-					# we could check for MIMETypes here
-					part = MIMEBase('application',"octet-stream")
-					part.set_payload(open(phile, "rb").read())
-					Encoders.encode_base64(part)
-					part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(phile))
-					msg.attach(part)
+			if attachments:
+				for phile in attachments:
+						# we could check for MIMETypes here
+						part = MIMEBase('application',"octet-stream")
+						part.set_payload(open(phile, "rb").read())
+						Encoders.encode_base64(part)
+						part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(phile))
+						msg.attach(part)
 			return msg
