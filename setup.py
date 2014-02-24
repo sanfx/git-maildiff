cc2ade924e1086fa8d53a2ad0b574b0c3eca873f
san <skysan@gmail.com>
2014-02-24 23:50:24 +0530

    added file to centrally update verison, updated setup.py to put VERSION file and renamed licence to LICENSE and added MANIFEST.in file

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
+from os import path
+
+v = open(path.join(path.dirname(__file__), 'VERSION'))
+VERSION = v.readline().strip()
+v.close()
 
 setup(
-    name='git-ipush',
-    version='1.0.0',
+    name='ipush',
+    version=VERSION,
     author='Sanjeev Kumar',
     author_email='skysan@gmail.com',
     packages=['mailer'],
+    data_files = ['VERSION'],
     scripts=['scripts/git-ipush'],
     url='https://bitbucket.org/sanfx/git-ipush',
-    license='licence',
+    license='LICENSE',
     description='Package to push and email color git diff',
     long_description=open('README.md').read(),
     entry_points={
